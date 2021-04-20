0807f4063de76dd8cecfa1857ff6a5da302c4f9a
kirubakaran4 <78558971+kirubakaran4@users.noreply.github.com>
2021-04-20 19:33:33 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 //Fibonacci using recursion
 
 
-class FibonacciExample1{  
+class Fibonacci1{  
 public static void main(String args[])  
 {    
  int n1=0,n2=1,n3,i,count=10;    
@@ -21,3 +21,17 @@ public static void main(String args[])
 
 
 //Fibonacci without recursion  
+
+
+public Fibonacci 2 {
+   public static void main(String args[]) {
+       int n1 = 0, n2 = 1, n3, i, max = 10;
+       System.out.print(n1 + " " + n2);
+       for (i = 2; i < max; ++i) {
+          n3 = n1 + n2;
+          System.out.print(" " + n3);
+          n1 = n2;
+          n2 = n3;
+       }
+    }
+ }
